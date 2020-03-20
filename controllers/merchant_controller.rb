786d66eb41750dd8d53c3b535775e70d3882f3a3
mike <>
2020-03-20 16:21:48 +0000

    sinatra running on app.rb

--- controllers/merchant_controller.rb
+++ controllers/merchant_controller.rb
@@ -3,12 +3,13 @@ require('sinatra/contrib/all')
 require_relative('../models/merchant.rb')
 also_reload('../models/*')
 
-get '/merchant' do
-  @merchant = Merchant.all()
-  erb (:"merchant/index")
+get '/merchants' do
+  @merchants = Merchant.all
+  erb (:"merchants/index")
 end
 
-get '/merchant/:id' do
-  @merchant = Merchant.find(params['id'].to_i)
-  erb(":merchant/show")
+
+
+get '/merchants/:id' do
+  
 end
