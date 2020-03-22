c63e4e16692740561ceb68169b944af9c61dd1fe
mike <>
2020-03-22 14:14:40 +0000

    map_items added to merchant

--- models/merchant.rb
+++ models/merchant.rb
@@ -40,14 +40,19 @@ class Merchant
     SqlRunner.run(sql, values)
   end
 
+  def tags()
+    sql = "SELECT tags.* FROM tags tags INNER JOIN transactions transactions ON transactions.tag_id = tag_id WHERE transactions.merchant_id = $1"
+    values = [@id]
+    merchant_data = SqlRunner.run(sql, values)
+    return Tag.map_items(merchant_data)
+  end
+
   def self.find( id )
     sql = "SELECT * FROM merchants WHERE id = $1"
      values = [id]
     results = SqlRunner.run(sql, values)
     return Merchant.new(results.first)
   end
-
-
 
   def self.all()
     sql = "SELECT * FROM merchants"
@@ -60,6 +65,9 @@ class Merchant
     SqlRunner.run(sql)
   end
 
-  
+  def self.map_items(merchant_data)
+    result = merchant_data.map { |merchant| Merchant.new(merchant)}
+    return result
+  end
 
 end
